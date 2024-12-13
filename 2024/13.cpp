a859b00222f630f480ea360e09767feb5e88d893
Bouyer François <francois.bouyer@thetribe.io>
2024-12-13 18:59:17 +0100

    optimisation parsin

--- 2024/13.cpp
+++ 2024/13.cpp
@@ -43,22 +43,6 @@ vector<Game> parseInput(const string& input) {
     vector<Game> games;
     vector<string> lines = splitString(input, '\n');
     Game game;
-    /// input example
-    // Button A: X+94, Y+34
-    // Button B: X+22, Y+67
-    // Prize: X=8400, Y=5400
-
-    // Button A: X+26, Y+66
-    // Button B: X+67, Y+21
-    // Prize: X=12748, Y=12176
-
-    // Button A: X+17, Y+86
-    // Button B: X+84, Y+37
-    // Prize: X=7870, Y=6450
-
-    // Button A: X+69, Y+23
-    // Button B: X+27, Y+71
-    // Prize: X=18641, Y=10279
 
     for (int i = 0; i < lines.size(); i += 4) {
         game.buttonA = parseCoordinate(lines[i]);
@@ -66,31 +50,6 @@ vector<Game> parseInput(const string& input) {
         game.price = parseCoordinate(lines[i + 2]);
         games.push_back(game);
     }
-
-
-
-    // string currentField = "buttonA";
-    
-    // for (const string& line : lines) {
-    //     if (line.empty()) {
-    //         games.push_back(game);
-    //         game = Game();
-    //         continue;
-    //     }
-        
-    //     if (currentField == "buttonA") {
-    //         currentField = "buttonB";
-    //         game.buttonA = parseCoordinate(line);
-    //     } else if (currentField == "buttonB") {
-    //         currentField = "price";
-    //         game.buttonB = parseCoordinate(line);
-    //     } else if (currentField == "price") {
-    //         game.price = parseCoordinate(line);
-    //         currentField = "buttonA";
-    //     }
-    // }
-    
-    // games.push_back(game);
 
     return games;
 }
