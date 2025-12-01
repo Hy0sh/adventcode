caba08372db0c40eda524a7081eebdd1c0bc2124
François Bouyer <francois.bouyer@gmail.com>
2025-12-01 11:51:33 +0100

    adding 2025/01

---
+++ 2025/01/main.ts
@@ -0,0 +1,29 @@
+import fs from 'fs';
+
+const input = fs.readFileSync('input.txt', 'utf8');
+
+const lines = input.split('\n').filter(line => line.length > 0);
+
+const solution = (lines: string[]) => {
+    let position = 50;
+    let solution1 = 0;
+    let solution2 = 0;
+    
+    for (const line of lines) {
+        const amount = parseInt(line.substring(1));
+
+        if (line[0] === 'R') {
+            solution2 += Math.floor((position + amount) / 100);
+            position = (position + amount) % 100;
+        } else {
+            solution2 += amount >= position || position === 0 ? Math.floor((amount - position) / 100) + Number(position !== 0) : 0;
+            position = (position - amount % 100 + 100) % 100;
+        }
+        
+        solution1 += Number(position === 0);
+    }
+
+    return {solution1, solution2};
+}
+
+console.log(solution(lines));
---
+++ 2025/run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+day=$1
+
+if [ -z "$day" ]; then
+    echo "Usage: $0 <day>"
+    exit 1
+fi
+
+cd ${day} && npx tsx main.ts
