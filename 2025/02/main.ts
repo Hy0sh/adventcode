8d3865f097a91dc4ef48a19ffa1b81ad02d05f5f
François Bouyer <francois.bouyer@gmail.com>
2025-12-02 08:46:33 +0100

    adding day 02

---
+++ 2025/02/main.ts
@@ -0,0 +1,49 @@
+import fs from 'fs';
+
+const input = fs.readFileSync('input.txt', 'utf8');
+
+const lines = input.split('\n').filter(line => line.length > 0);
+
+const solution = (lines: string[]) => {
+    let solution1 = 0;
+    let solution2 = 0;
+    
+    const coupleIds = lines.map(line => line.split(',').map(ids => ids.split('-').map(Number))).flat();
+
+    for (const coupleId of coupleIds) {
+        const [id1, id2] = coupleId;
+        for(let i = id1; i <= id2; i++) {
+            const iString = i.toString();
+            if(iString.length %2 === 0 && iString.substring(0, iString.length / 2) === iString.substring(iString.length / 2)) {
+                solution1 += i;
+            }
+
+            if(isInvalidId(i)) {
+                solution2 += i;
+            }
+        }
+    }
+
+    return {solution1, solution2};
+}
+
+function isInvalidId(id: number) {
+    const idString = id.toString();
+    
+    for(let i = 0; i < idString.length; i++) {
+        if(i > idString.length / 2) {
+            return false;
+        }
+        const subIdString = idString.substring(0, i);
+        if(idString.length % subIdString.length !== 0) {
+            continue;
+        }
+        const nbPossiblePattern = idString.length / subIdString.length;
+        if(nbPossiblePattern > 1 && idString.includes(subIdString.repeat(nbPossiblePattern))) {
+            return true;
+        }
+    }
+    return false;
+}
+
+console.log(solution(lines));
